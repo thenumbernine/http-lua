8d53c12716269e88b7ebcf25608ed719924dc7be
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-11-08 00:26:45 +0300

    better dir listing

--- http.lua
+++ http.lua
@@ -34,10 +34,32 @@ while true do
 			local attr = lfs.attributes(localfilename)
 			if attr and attr.mode == 'directory' then
 				assert(client:send('HTTP/1.1 200/OK\r\nContent-Type:text/html\r\n\r\n'))
+				assert(client:send('<h3>Index of '..filename..'</h3>\n'))
+				assert(client:send('<html>\n'))
+				assert(client:send('<head>\n'))
+				assert(client:send('<title>Directory Listing of '..filename..'</title>\n'))
+				assert(client:send('<style type="text/css"> td{padding-right:20px};</style>\n'))
+				assert(client:send('</head>\n'))
+				assert(client:send('<body>\n'))
+				assert(client:send('<table>\n'))
+				assert(client:send('<tr><th>Name</th><th>Modified</th><th>Size</th></tr>\n'))
 				for file in lfs.dir(localfilename) do
-					local nextfilename = (filename..'/'..file):gsub('//', '/')
-					assert(client:send('<a href="'..nextfilename..'">'..file..'</a><br>\n'))
+					if file ~= '.' then
+						local nextfilename = (filename..'/'..file):gsub('//', '/')
+						local displayfile = file
+						local subattr = lfs.attributes(localfilename..'/'..file)
+						if subattr and subattr.mode == 'directory' then
+							displayfile = '[' .. displayfile .. ']'
+						end
+						assert(client:send('<tr>'))
+						assert(client:send('<td><a href="'..nextfilename..'">'..displayfile..'</a></td>'))
+						assert(client:send('<td>'..(subattr and os.date('%F %T',subattr.modification) or '')..'</td>'))
+						assert(client:send('<td style="text-align:center">'..(subattr and (subattr.mode == 'directory' and '-' or subattr.size) or '')..'</td>'))
+						assert(client:send('</tr>\n'))
+					end
 				end
+				assert(client:send('</table>\n'))
+				assert(client:send('</body>\n'))
 			else
 				local result = file[localfilename]
 				if result then
